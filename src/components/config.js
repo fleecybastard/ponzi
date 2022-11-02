88fec723db44da805e413c4a70c206338184b278
Illia <illia09092003@gmail.com>
2022-11-02 19:18:25 +0200

    edited links

--- src/components/config.js
+++ src/components/config.js
@@ -2,11 +2,11 @@ export const LINKS = {
     UNISWAP: '',
     CHART: '',
     PONZI_GAME: '',
-    TELEGRAM: '',
-    TWITTER: '',
+    TELEGRAM: 'https://t.me/xPonzi',
+    TWITTER: 'https://twitter.com/xPonzii',
     ETHERSCAN: '',
-    GITHUB: '',
-    MEDIUM: '',
+    GITHUB: 'https://github.com/xPonzi',
+    MEDIUM: 'https://medium.com/@xPonzi',
 }
 
 export const CONTRACT_ADDRESS = '0x654f8C82217d4a5d0Ad37B4D8bf1130Dfdf52eb9'
